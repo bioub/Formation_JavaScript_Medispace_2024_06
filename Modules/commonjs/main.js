9d9149a168f4704364d9027353909d97b5be7895
Romain Bohdanowicz <romain.bohdanowicz@formation.tech>
2024-06-18 15:25:51 +0200

    CommonJS

---
+++ Modules/commonjs/main.js
@@ -0,0 +1,6 @@
+const hello = require('./hello.js');
+const MyMaths = require('./my-maths.js');
+
+console.log(hello('Romain'));
+console.log(MyMaths.sum(1, 2));
+console.log(MyMaths.multiply(1, 2));
