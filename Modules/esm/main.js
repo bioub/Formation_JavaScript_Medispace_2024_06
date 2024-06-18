74609e12195bd18f8769cc8b33be9ce633580371
Romain Bohdanowicz <romain.bohdanowicz@formation.tech>
2024-06-18 16:29:49 +0200

    Exercices

---
+++ Modules/esm/main.js
@@ -0,0 +1,6 @@
+import hello from './hello.js';
+import { sum, multiply } from './my-maths.js';
+
+console.log(hello('Romain'));
+console.log(sum(1, 2));
+console.log(multiply(1, 2));
---
+++ Modules/ex-commonjs/jeu.js
@@ -0,0 +1,46 @@
+// importer getRandomInt
+const readline = require('readline');
+
+class Jeu {
+  rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  essais = [];
+  constructor(options = {}) {
+    const min = options.min ?? 0;
+    const max = options.max ?? 100;
+    this.entierAlea = getRandomInt(min, max);
+  }
+  jouer() {
+    if (this.essais.length) {
+      console.log(`Vous avez déjà joué : ${this.essais.join(' - ')}`);
+    }
+
+    this.rl.question('Quel est le nombre entier ? ', (answer) => {
+      console.log(`Vous avez saisi : ${answer}`);
+
+      const entierSaisi = Number.parseInt(answer, 10);
+
+      if (Number.isNaN(entierSaisi)) {
+        console.log('Vous devez saisi un nombre');
+        return this.jouer();
+      }
+
+      this.essais.push(entierSaisi);
+
+      if (entierSaisi > this.entierAlea) {
+        console.log('Trop grand');
+        this.jouer();
+      } else if (entierSaisi < this.entierAlea) {
+        console.log('Trop petit');
+        this.jouer();
+      } else {
+        console.log('Gagné');
+        this.rl.close();
+      }
+    });
+  }
+}
+
+// exporter Jeu
