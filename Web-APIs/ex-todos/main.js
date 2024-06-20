6117c42aad59b01fd30e8c858e30cb51689139bd
Romain Bohdanowicz <romain.bohdanowicz@formation.tech>
2024-06-20 12:02:58 +0200

    Exercices

--- Web-APIs/ex-todos/main.js
+++ Web-APIs/ex-todos/main.js
@@ -36,3 +36,14 @@ toggleEl.addEventListener('click', () => {
     checkboxEl.checked = toggleEl.checked;
   }
 })
+
+// Exercice : Event Phases
+// En utilisant la propagation et
+// event.target
+// Déplacer le addEventListener du bouton moins ici
+// en écouter le click de listEl
+// Si event.target correspond à un bouton moins (en testant la class par exemple)
+// supprimer le closest ancestor (avec closest idéalement) qui matche
+// la classe .todo-item
+
+// Faire de même pour le dblclick et keydown
